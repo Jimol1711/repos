0162e4b7c3e80a0a338b38abf5f2a888cc18f956
Jimol1711 <juan.molina.l@ug.uchile.cl>
2024-12-14 19:19:45 -0300

    small update

--- repo_updater.py
+++ repo_updater.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 
 def update_gitignore():
-    """Update the .gitignore file in the current directory with all files and directories except the script file."""
+    """Update the .gitignore file in the current directory with all files and directories except the script file and READMEs."""
     current_dir = os.getcwd()
     script_name = os.path.basename(__file__)
     gitignore_path = os.path.join(current_dir, ".gitignore")
